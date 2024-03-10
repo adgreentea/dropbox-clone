d95199acb7cf3b318bce57b6c3b19414494d2a70
LitoHDD <sergio098890@gmail.com>
2024-03-10 01:42:14 +0100

    :construction: Work in pogress: implement table wrapper component

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { FileType } from "@/typings";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { columns } from "./Columns";
 import { DataTable } from "./Table";
+import { useUser } from "@clerk/nextjs";
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [initialFiles, setInitialFiles] = useState<FileType[]>([]);
+  const [sort, setSort] = useState<"asc" | "desc">("desc");
   return (
-    <div>
-      <Button>Sort By ...</Button>
+    <div onClick={() => setSort(sort === "desc" ? "asc" : "desc")}>
+      <Button>Sort By {sort === "desc" ? "Newest" : "Oldest"}</Button>
       <DataTable columns={columns} data={skeletonFiles} />
     </div>
   );
